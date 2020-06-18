e952be2eba443b2be42aa0530b152be21c103451
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-06-18 11:18:31 -0500

    refact: create root index

---
+++ index.js
@@ -0,0 +1,13 @@
+import Table from './core/entities/Table.js'
+import Nodule from './core/entities/Nodule.js'
+import FilterNodule from './core/entities/nodules/FilterNodule.js'
+import JoinNodule from './core/entities/nodules/JoinNodule.js'
+import TransformNodule from './core/entities/nodules/TransformNodule.js'
+
+export {
+  Table,
+  Nodule,
+  FilterNodule,
+  JoinNodule,
+  TransformNodule
+}
