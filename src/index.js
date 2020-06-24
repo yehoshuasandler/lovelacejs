23961c4778841f0fc0a1d508ca1d6eccbd9dcf87
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-06-23 20:32:45 -0500

    refact: bundle package

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Table from './entities/Table.js'
+import Nodule from './entities/Nodule.js'
+import FilterNodule from './entities/nodules/FilterNodule.js'
+import JoinNodule from './entities/nodules/JoinNodule.js'
+import TransformNodule from './entities/nodules/TransformNodule.js'
+
+export {
+  Table,
+  Nodule,
+  FilterNodule,
+  JoinNodule,
+  TransformNodule
+}
+
+export default {
+  Table,
+  Nodule,
+  FilterNodule,
+  JoinNodule,
+  TransformNodule
+}
